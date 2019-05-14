c0fe9d81bcbde4e25bf08acccd7309dbb769beeb
League Student <league@iMac-11.attlocal.net>
2019-05-13 18:28:47 -0700

    I don't remember
    wish i did

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -1,7 +1,7 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
@@ -17,15 +17,22 @@ public class ChangeCalculator {
 		// Ask the user how many nickels they have
 
 		// Convert their answer to an int using Integer.parseInt()
+		String p = JOptionPane.showInputDialog("how many pennies do you have");
 
+		double P = Integer.parseInt(p);
+		String n = JOptionPane.showInputDialog("how many nickles do you have");
+
+		double N = Integer.parseInt(n);
 		// Ask the user how many dimes they have, and convert their answer
+		String d = JOptionPane.showInputDialog("how many dimes do you have");
 
+		double D = Integer.parseInt(d);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+		String q = JOptionPane.showInputDialog("how many quarters do you have");
+		double Q = Integer.parseInt(q);
+		// Calculate how much money the user has and save it in a double variable
+		double $ = P / 100 + N / 20 + D / 10 + Q / 4;
 		// Tell the user how much money they have
-
+		JOptionPane.showMessageDialog(null, "you have $" + $);
 	}
 }
-
