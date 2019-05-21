1dd5a7653d29911a0687e22189d68b9e9aa8c8a0
League Student <league@iMac-11.attlocal.net>
2019-05-20 18:27:57 -0700

    nothing

--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -4,20 +4,27 @@ package extra;
 import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
+	
 	public static void main(String[] args) {
 
+	}
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+String a = JOptionPane.showInputDialog("num1");
+String b = JOptionPane.showInputDialog("num2");
+int num1 = Integer.parseInt(a);
+int num2 = Integer.parseInt(b);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+		int operation = JOptionPane.showOptionDialog(null, "What opperator", "", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
 
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
-
+	private void add() {
+		
+	}
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
